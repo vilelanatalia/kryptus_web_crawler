bc04eb39cab4ad32b7edaf76bf32219a893850ce
Natalia <nataliavilela79@gmail.com>
2022-05-11 23:03:20 -0300

    adicionado documentacoa dos metodos

--- README.md
+++ README.md
@@ -12,10 +12,9 @@ Um web crawler que varre os dados de uma determinada lista de produtos no mercad
 Tendo-os instalados, basta executar o projeto no terminal ou na IDLE
 
 ##Conclusões 
-    - Ao que se parece, a configuração dos elementos html mudam de acordo com a pesquisa, então não é possível fazer o web scraping em qualquer pesquisa. Existem pelo menos dois tipos de html apresentados ao realizar a pesquisa de um produto no mercado livre 
+    - Ao que se parece, a configuração dos elementos html mudam de acordo com a pesquisa, então não é possível fazer o web scraping em qualquer pesquisa. Percebi que existem pelo menos dois tipos de html apresentados ao realizar a pesquisa de um produto no mercado livre e por isso que dependendo do html, o programa não funciona corretamente. 
 
 ##Observações
-
     - Não consegui fazer o web crawler percorrer as 5 páginas
 
 
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def createJson(datas):
         outfile.write(json_object)
 
 def hasFreeShipping(string_text):
+    '''Verifica se a loja possui frete gratis'''
     try:
         string_text.text
         return True
@@ -15,6 +16,7 @@ def hasFreeShipping(string_text):
         return False
 
 def processStoreName(string_text):
+    '''Trata o dado bruto do nome da loja, retornando apenas o nome da loja para o arquivo json'''
     try:
         new_string = string_text.text.split(' ')
         del(new_string[0])
@@ -22,26 +24,28 @@ def processStoreName(string_text):
     except:
         return "NOME_DA_LOJA_NAO_FORNECIDO"
 
-def convertFloat(string_value):
-    return float(string_value)
-
 def processPriceValue(string_text):
+    '''Trata o dado bruto de preços, retirando da string valores não interessantes e convertendo-o para o tipo float'''
     try:
         new_string = string_text.replace("R$", ",").split(',')
         del(new_string[0])
-        return convertFloat(new_string[0]+ '.'+ new_string[1])
+        return float(new_string[0]+ '.'+ new_string[1])
     except:
         return 0.0
 
 def processAmountParcel(string_text):
+    '''Trata o dado bruto do numero de parcela, retirando da string valores não interessantes e convertendo-o para o tipo float'''
     new_string = string_text.replace("em", "x").split('x')
-    return convertFloat(new_string[1])
+    return float(new_string[1])
 
 def parcelValue(valueParcel, amountParcel ):
+    '''Multiplica o valor da parcela e a quantidade de parcelas para retornar o valor total da parcela'''
     valueTotal = valueParcel * amountParcel
     return valueTotal
 
 def getProduct(search_link):
+    '''Método principal, busca pelos elementos, ainda brutos, no html da página e depois recebe o
+    refinamento de dados para serem agrupados em uma array '''
     dados = []
     products = search_link.find_all('div', attrs={'class': 'ui-search-result__wrapper'})
 
